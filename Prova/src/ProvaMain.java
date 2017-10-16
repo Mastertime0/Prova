9c5f78396432f357ca2fc07156ed7036aee0457d
Alunno <Alunno@PC-25-INF4>
2017-10-16 12:21:53 +0200

    Ho aggiunto le grills

--- Prova/src/ProvaMain.java
+++ Prova/src/ProvaMain.java
@@ -4,8 +4,7 @@ public class ProvaMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("viva i memes");
-		
+		System.out.println("viva i memes e le anime grill");
 	}
 
 }
